989c568314f5c6ffc589263eb10a31ab09830ecc
antonplkv <polyakov.anton@ukr.net>
2019-11-15 21:56:11 +0200

    fix url

--- web_shop_bot/config.py
+++ web_shop_bot/config.py
@@ -1,6 +1,6 @@
 TOKEN = '1038706967:AAEWTCAQbiudnckb-3dllDlaJLvZ03BgopU'
 host = '127.0.0.1'
-webhook_url = f'https://35.224.97.111/{TOKEN[::-1]}'#set webhook for this url
+webhook_url = 'https://35.224.97.111'#set webhook for this url
 handle_url = f''
 
 print(TOKEN[::-1])
